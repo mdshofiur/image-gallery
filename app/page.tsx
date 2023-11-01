6d5c38f45453a09f5f8b8dcab35742251f130ad5
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-11-01 14:58:04 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { LargeFirstTile } from "@/components/gallery/MainFile";
+
+const Page = () => {
+  return (
+    <main className="container mx-auto min-h-screen py-10">
+      <LargeFirstTile />
+    </main>
+  );
+};
+
+export default Page;
